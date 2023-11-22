031ecb9f2e5a7c2c3a099a7aaffe006f22241bc2
unknown <sajalsahu06@gmail.com>
2023-11-22 23:41:28 +0530

    update

--- src/components/HeaderNew.js
+++ src/components/HeaderNew.js
@@ -22,9 +22,9 @@ function HeaderNew() {
     }, []);
     return (
         <header>
-            <nav class="navbar navbar-expand-lg">
-                <div class="container">
-                    <a class="navbar-brand text-white" href="#">
+            <nav className="navbar navbar-expand-lg">
+                <div className="container">
+                    <a className="navbar-brand text-white" href="#">
                       <img src={logoheader} alt="img" style={{ width: "60px", padding: "9px" }} />
                       <span
                              style={{
@@ -48,55 +48,55 @@ function HeaderNew() {
                         </span>
                       </a>
                         
-                           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nvbCollapse" aria-controls="nvbCollapse">
-                        <span class="navbar-toggler-icon"></span>
+                           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#nvbCollapse" aria-controls="nvbCollapse">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="nvbCollapse">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item pl-1">
-                                <a class="nav-link" href="/"><i class="fa fa-home fa-fw mr-1"></i>Home</a>
+                    <div className="collapse navbar-collapse" id="nvbCollapse">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item pl-1">
+                                <a className="nav-link" href="/"><i className="fa fa-home fa-fw mr-1"></i>Home</a>
                             </li>
 
                             {
                                 role === 'admin' ?
                                     <>
-                                        <li class="nav-item pl-1">
-                                            <a class="nav-link" href="/admin"><i class="fa fa-th-list fa-fw mr-1"></i>Dashboard</a>
+                                        <li className="nav-item pl-1">
+                                            <a className="nav-link" href="/admin"><i className="fa fa-th-list fa-fw mr-1"></i>Dashboard</a>
                                         </li>
                                     </> : null
                             }
 
                             {role === 'user' ?
                                 <>
-                                    <li class="nav-item pl-1">
-                                        <a class="nav-link" href="/get"><i class="fa fa-th-list fa-fw mr-1"></i>Search Reports</a>
+                                    <li className="nav-item pl-1">
+                                        <a className="nav-link" href="/get"><i className="fa fa-th-list fa-fw mr-1"></i>Search Reports</a>
                                     </li>
-                                    <li class="nav-item pl-1">
-                                        <div class="dropdown">
-                                            <button class="btn nav-link" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={{ background: "#000000", opacity: "0.6" }}>
-                                                <i class="fa fa-th-list fa-fw mr-1"></i>Services
+                                    <li className="nav-item pl-1">
+                                        <div className="dropdown">
+                                            <button className="btn nav-link" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={{ background: "#000000", opacity: "0.6" }}>
+                                                <i className="fa fa-th-list fa-fw mr-1"></i>Services
                                             </button>
-                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                                <a class="dropdown-item" href="/get">Lab Reports</a>
-                                                <a class="dropdown-item" href="/appoinment">Channeling</a>
-                                                <a class="dropdown-item" href="/add">Inquiry </a>
-                                                <a class="dropdown-item" href="/addpriscription">Buy Medicine</a>
-                                                <a class="dropdown-item" href="/videoconsultant">Video Consultant</a>
-                                                <a class="dropdown-item" href="/saerchOrder">Search Order</a>
-                                                <a class="dropdown-item" href="/paymentportal">Payment</a>
+                                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <a className="dropdown-item" href="/get">Lab Reports</a>
+                                               {/* * <a className="dropdown-item" href="/appoinment">Channeling</a>* */}
+                                                <a className="dropdown-item" href="/add">Inquiry </a>
+                                                <a className="dropdown-item" href="/addpriscription">Buy Medicine</a>
+                                                <a className="dropdown-item" href="/videoconsultant">Video Consultant</a>
+                                                <a className="dropdown-item" href="/saerchOrder">Search Order</a>
+                                                {/* <a className="dropdown-item" href="/paymentportal">Payment</a> */}
                                             </div>
                                         </div>
                                     </li> </> : null}
 
-                            <li class="nav-item pl-1">
+                            <li className="nav-item pl-1">
                                 {localStorage.getItem("user") ?
-                                    <div class="dropdown">
+                                    <div className="dropdown">
                                         <button type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={{ background: 'transparent', border: "none" }}>
                                             <PersonIcon sx={{ color: "white" }} fontSize="large" />
                                         </button>
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                            <a class="dropdown-item" href={'/viewPatient/' + user._id}>Profile</a>
-                                            <button type="button" class="dropdown-item" onClick={() => logOut()}>LogOut</button>
+                                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <a className="dropdown-item" href={'/viewPatient/' + user._id}>Profile</a>
+                                            <button type="button" className="dropdown-item" onClick={() => logOut()}>LogOut</button>
                                         </div>
                                     </div>
                                     : <a className="btn btn-primary ml-5" href='/login'>login</a>}
